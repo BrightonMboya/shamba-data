effed7617b794e4b4f4300b2ee591c5355c8b1e8
tony <b.mboya@alustudent.com>
2022-11-29 18:06:58 +0200

    fixing the OG-Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    astroOGImage({
+      config: {
+        path: "/public", // change this value to the folder where your posts are
+        // NOTE: index.md file will not get proccesed, so please avoid it
+      },
+    }),
+  ],
 });
