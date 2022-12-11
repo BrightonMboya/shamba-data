b425ec6c3d918dfac448869358bf8f5f10750faa
tony <b.mboya@alustudent.com>
2022-12-11 16:18:12 +0200

    added responsiveness on md and lg screens

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    // astroOGImage({
-    //   config: {
-    //     path: "/public", // change this value to the folder where your posts are
-    //     // NOTE: index.md file will not get proccesed, so please avoid it
-    //   },
-    // }),
-  ],
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
---
+++ todo.md
@@ -0,0 +1,4 @@
+## Some Things not to Forget
+
+1. Populate Links and screens for the Footer
+2. Add A formLink to collect emails in the Footer
